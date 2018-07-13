66f9451f9f7dd6ed8bfe4ecc6ba6fe645717cee1
bedjan <29821749+bedjan@users.noreply.github.com>
2018-07-13 16:22:53 +0200

    Update index.php

--- index.php
+++ index.php
@@ -4,5 +4,9 @@ $url = file_get_contents
 
 ('https://raw.githubusercontent.com/bedjan/web_home/master/obsah_bednar.html');
 echo $url; 
+$url = file_get_contents
+
+('https://raw.githubusercontent.com/bedjan/web_home/master/obsah_bednar.html');
+echo $url; 
 
 ?>
